7b6955ef4c7cf6031183a8c59eb64b71c06e837d
SimonBoothroyd <simon.boothroyd@colorado.edu>
2021-01-02 12:03:03 +0000

    Adds initial launch cli.

--- inspector/tests/cli/test_cli.py
+++ inspector/tests/cli/test_cli.py
@@ -1,4 +1,5 @@
 from inspector.cli import cli as root_cli
+from inspector.cli.cli import launch_cli
 
 
 def test_root_cli(runner):
@@ -7,3 +8,24 @@ def test_root_cli(runner):
 
     if result.exit_code != 0:
         raise result.exception
+
+    assert "launch" in result.output
+
+
+def test_launch_cli(monkeypatch, runner):
+
+    import uvicorn
+
+    expected_message = "Start Server"
+
+    def mock_run(*args, **kwargs):
+        print(expected_message)
+
+    monkeypatch.setattr(uvicorn, "run", mock_run)
+
+    result = runner.invoke(launch_cli)
+
+    if result.exit_code != 0:
+        raise result.exception
+
+    assert f"{expected_message}\n" == result.output
